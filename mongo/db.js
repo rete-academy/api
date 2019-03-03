53fc16d036b45e8d47a54646842cf48c97fef178
Sang Dang <sangdth@gmail.com>
2019-03-03 03:28:47 +0200

    check mongo connection with error handling

--- mongo/db.js
+++ mongo/db.js
@@ -10,28 +10,24 @@ const {
 mongoose.set('useCreateIndex', true);
 
 // Establish database connection
-mongoose.connect(
-    process.env.MONGO_URL,
-    {
-        user: process.env.MONGO_USER,
-        pass: process.env.MONGO_PASS,
-        useNewUrlParser: true ,
-    },
-);
+handleConnect();
 
 // Listen for Mongoose connection events and output statuses to console
-mongoose.connection.on('connected', function () {
+mongoose.connection.on('connected', function() {
     log.verbose('Connecting to ' + process.env.MONGO_URL);
     log.info('App connected to database successfully');
     createFirstUser();
     createFirstClient();
 });
 
-mongoose.connection.on('error', function (err) {
+mongoose.connection.on('error', function(err) {
     log.error(err);
+    log.info('Trying to reconnect...');
+
+    handleConnect();
 });
 
-mongoose.connection.on('disconnected', function () {
+mongoose.connection.on('disconnected', function() {
     log.info('App disconnected from database');
 });
 
@@ -39,28 +35,43 @@ mongoose.connection.on('disconnected', function () {
 // and call gracefulShutdown function when appropriate,
 // passing a continuation callback
 // To be called when process is restarted or terminated
-let gracefulShutdown = function (msg, callback) {
+function gracefulShutdown(msg, callback) {
     mongoose.connection.close(function () {
         callback();
     });
-};
+}
+
+function handleConnect() {
+    mongoose.connect(
+        process.env.MONGO_URL,
+        {
+            user: process.env.MONGO_USER,
+            pass: process.env.MONGO_PASS,
+            useNewUrlParser: true ,
+        },
+    );
+
+    mongoose.connection.once('open', function() {
+        log.info('We are connected to DB');
+    });
+}
 
 // Listen for SIGUSR2, which is what nodemon uses to restart
-process.once('SIGUSR2', function () {
+process.once('SIGUSR2', function() {
     gracefulShutdown('nodemon restart', function () {
         process.kill(process.pid, 'SIGUSR2');
     });
 });
 
 // Listen for SIGINT emitted on application termination
-process.on('SIGINT', function () {
+process.on('SIGINT', function() {
     gracefulShutdown('app termination', function () {
         process.exit(0);
     });
 });
 
 // Listen for SIGTERM emitted by Heroku and Jelastic for shutdown
-process.on('SIGTERM', function () {
+process.on('SIGTERM', function() {
     gracefulShutdown('Heroku app termination', function () {
         process.exit(0);
     });
