e4c202c1fb9b539302e1ccfaa5b68bc3ba0ba53e
Sang Dang <sangdth@gmail.com>
2020-07-31 20:24:47 +0300

    damn 3

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
-// const io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
 sentry.init({ dsn: 'https://7a23e0719d964b6ea1252e9912d5e0f0@sentry.io/1401406' });
 
@@ -42,7 +42,10 @@ require('mongo/db');
 app.use(helmet);
 
 // app.use(cors({ origin: config.default.webUrl }));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
 
 const sessOptions = {
   secret: 'sessionSecret123',
@@ -59,13 +62,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(passport.initialize());
 app.use((req, res, next) => {
-  // req.io = io; // add io into req so controllers can use it
-
-  res.header('Access-Control-Allow-Origin', '*'); // Learn about this later
-  // res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
+  req.io = io; // add io into req so controllers can use it
   next();
 });
 
