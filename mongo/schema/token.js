00e3082a1fda76014b3a7f094e6b444001af6bf1
Sang Dang <sangdth@gmail.com>
2020-07-31 16:57:39 +0300

    small fix

--- mongo/schema/token.js
+++ mongo/schema/token.js
@@ -1,23 +1,21 @@
-'use strict';
-
 module.exports = {
   expires: Date,
   scope: String,
   created_time: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   user_id: {
     type: String,
-    required: true
+    required: true,
   },
   client_id: {
     type: String,
-    required: true
+    required: true,
   },
   token: {
     type: String,
     unique: true,
-    required: true
+    required: true,
   },
 };
